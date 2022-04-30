51460f257ded7bf2a85545c2c798fdc84ad931f6
Vishal Brahmbhatt <vdb2@njit.edu>
2022-04-30 00:11:24 -0400

    trying to fix pytest issue in git

--- tests/user_test.py
+++ tests/user_test.py
@@ -51,21 +51,21 @@ def test_uploading_files(application):
         assert db.session.query(User).count() == 0
         assert db.session.query(Song).count() == 0
 
-    # root = config.Config.BASE_DIR
-    Filecsv = 'music.csv'
-    Flupload = config.Config.UPLOAD_FOLDER
-    uploadFile = os.path.join(Flupload, Filecsv)
-    uploadFl = os.path.join(Flupload)
-    assert os.path.exists(uploadFl) is True
-    with application.test_client() as client:
-        with open(uploadFile, 'rb') as file:
-            data = {
-                'file': (file, Filecsv),
+        # root = config.Config.BASE_DIR
+        Filecsv = 'music.csv'
+        Flupload = config.Config.UPLOAD_FOLDER
+        uploadFile = os.path.join(Flupload, Filecsv)
+        uploadFl = os.path.join(Flupload)
+        assert os.path.exists(uploadFl) is True
+        with application.test_client() as client:
+            with open(uploadFile, 'rb') as file:
+                data = {
+                    'file': (file, Filecsv),
 
-            }
-            resp = client.post('songs/upload', data=data, follow_redirects=True)
+                }
+                resp = client.post('/songs/upload', data=data, follow_redirects=True)
 
-    assert resp.status_code == 400
+        assert resp.status_code == 400
 
 
 # def user_dashboard_access_approved(client):
